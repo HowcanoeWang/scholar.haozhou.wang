8a2d06e871d78a7b880d5e1810cce533d7c74782
浩瀚猫 <howcanoewang@gmail.com>
2025-05-27 00:05:52 +0900

    update workflow

--- py/get_google_scholar.py
+++ py/get_google_scholar.py
@@ -3,10 +3,16 @@ import json
 from scholarly import scholarly
 
 author = scholarly.search_author_id('CnTTa3EAAAAJ')
+print(author)
 
 sout = scholarly.fill(author, sections=['indices', 'counts'])
+print(sout)
 
 # only save 'citedby', 'citedby5y', 'hindex', 'hindex5y', 'i10index', 'i10index5y', 'coauthors', 'cites_per_year' to json file
 sout = {k: sout[k] for k in ('citedby', 'citedby5y', 'hindex', 'hindex5y', 'i10index', 'i10index5y', 'cites_per_year')}
+
+print("start saving json")
 with open('../files/scholar.json', 'w') as f:
     json.dump(sout, f)
+    
+print("json saved")
