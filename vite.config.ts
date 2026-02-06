98fbd74c66feee94074e4ce63463d6a9a016e36f
浩瀚猫 <howcanoewang@gmail.com>
2026-02-06 23:02:20 +0900

    build(init): init project

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   // 用于判断是用户根目录xxx.github.io还是子目录xxx.github.io/project-name）
   // 如果你自定义了域名 custom domain, 直接注释掉下面的base行
   base: process.env.VITE_BASE_PATH || '/', 
-  plugins: [tailwindcss(), svelte()],
+  plugins: [
+    tailwindcss(),
+    svelte()
+  ],
+  resolve: {
+    alias: {
+      $lib: path.resolve(__dirname, './src/lib'),
+    },
+  },
 })
