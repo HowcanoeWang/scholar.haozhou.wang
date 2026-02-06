b55e47b0cf79ea11209ac27d151895974c3f3f48
浩瀚猫 <howcanoewang@gmail.com>
2026-02-06 23:40:38 +0900

    fix(html): allow html text render

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -4,15 +4,15 @@
 
 <div class="pt-3">
     <!-- Intro -->
-    <p class="text-lg mt-3 mb-0" >
+    <p class="text-xl mt-3 mb-0" >
         {$t('prof.intro')}
     </p>
 
     <!-- Research Interests -->
-    <p class="text-lg mt-5 mb-0 font-bold">
+    <p class="text-xl mt-5 mb-0 font-bold">
         {$t('prof.int.t')}
     </p>
-    <ul class="list-disc ml-5 mt-2 mb-0 font-light">
+    <ul class="text-lg list-disc ml-5 mt-2 mb-0 font-light">
         <li>{$t('prof.int.l1')}</li>
         <li>{$t('prof.int.l2')}</li>
         <li>{$t('prof.int.l3')}</li>
@@ -20,21 +20,21 @@
     </ul>
 
     <!-- Education -->
-    <p class="text-lg mt-5 mb-0 font-bold">
+    <p class="text-xl mt-5 mb-0 font-bold">
         {$t('prof.edu.t')}
     </p>
     <ul class="mt-2 mb-0 font-light space-y-2">
         <li>
             <span>{$t('prof.edu.l1')}</span> 
-            <span class="badge badge-outline ml-2">2023.09</span>
+            <span class="badge bg-white text-black border-none ml-2">2023.09</span>
         </li>
         <li>
             <span>{$t('prof.edu.l2')}</span> 
-            <span class="badge badge-outline ml-2">2019.12</span>
+            <span class="badge bg-white text-black border-none ml-2">2019.12</span>
         </li>
         <li>
             <span>{$t('prof.edu.l3')}</span> 
-            <span class="badge badge-outline ml-2">2017.06</span>
+            <span class="badge bg-white text-black border-none ml-2">2017.06</span>
         </li>
     </ul>
 </div>
