f07d9758a295d070b5050b5aecd938a730b2b9fd
浩瀚猫 <howcanoewang@gmail.com>
2026-02-08 01:05:06 +0900

    build(host): custom domain deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from 'path'
 export default defineConfig({
   // 用于判断是用户根目录xxx.github.io还是子目录xxx.github.io/project-name）
   // 如果你自定义了域名 custom domain, 直接注释掉下面的base行
-  base: process.env.VITE_BASE_PATH || '/', 
+  // base: process.env.VITE_BASE_PATH || '/', 
   plugins: [
     tailwindcss(),
     svelte()
